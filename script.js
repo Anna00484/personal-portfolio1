5f352c69b959c2b15c6a4c0e3ac1aae39e10da67
Anna00484 <142963536+Anna00484@users.noreply.github.com>
2025-06-06 16:29:01 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thanks for your message! I’ll get back to you soon.');
+  this.reset();
+});
